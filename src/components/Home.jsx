4e0a2bda8dcee2c431e01f65a4aea085711e8ba5
Garoseau <garoseau.pro@gmail.com>
2022-06-14 03:28:26 +0200

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
 			<div class=''>
 			<h6 className='textBaseline, grey'>Bonjour, je m'appelle</h6>
 			<h1 className='textBaseline,'>Julien Gomes</h1>
-			<h2 className='textBaseline, grey'>Je suis actuellement étudiant en développement web. Je suis actuellement en plein travail sur ce petit portfolio </h2>
+			<h2 className='textBaseline, grey'>Je suis étudiant en développement web. <br /> Je suis actuellement en plein travail sur ce petit portfolio alors n'hésite pas revenir plus tard !</h2>
 			</div>
 		</div>
 	)
